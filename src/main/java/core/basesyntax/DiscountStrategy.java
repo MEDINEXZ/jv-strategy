17ebfeb26b1029ecf7a31c601adf623ebbf0f19e
Stepan Liakhovskiy <s.lyakhovskiy06@gmail.com>
2026-09-19 20:32:43 +0200

    Some structural fixes

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -9,14 +9,14 @@ import core.basesyntax.strategy.impl.NewYearDiscountService;
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-             case "New Year":
-                 return new NewYearDiscountService();
-             case "Birthday":
-                 return new BirthdayDiscountService();
-             case "Black Friday":
-                 return new BlackFridayDiscountService();
-             default:
+            case "New Year":
+                return new NewYearDiscountService();
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            default:
                 return new DefaultDiscountService();
-       }
-   }
+        }
+    }
 }
